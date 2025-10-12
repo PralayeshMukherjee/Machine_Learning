701710fbe55835778a2e20c8e0081dfad633d4a9
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 20:55:30 +0530

    change the url

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
@@ -3,7 +3,7 @@ package com.example.ML.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class PredictionService {
-    String url = "http://localhost:5000/predict";
+    String url = "http://localhost:5000";
     WebClient webClient = WebClient.create(url);
     String response = webClient.post()
             .uri("/predict")
