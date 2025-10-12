1dbeb8aca67acba9a9f0dd784163fd992584bc74
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 21:01:01 +0530

    create the parameterize constructor for the DTO layer

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/DTO/PredictionResponseDTO.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/DTO/PredictionResponseDTO.java
@@ -2,4 +2,8 @@ package com.example.ML.DTO;
 
 public class PredictionResponseDTO {
     private int prediction;
+
+    public PredictionResponseDTO(int prediction) {
+        this.prediction = prediction;
+    }
 }
