44af7bc2ee7f6a30728cddd4cc387044afe0c7b8
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 20:55:08 +0530

    add the uri mapping

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
@@ -5,4 +5,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class PredictionService {
     String url = "http://localhost:5000/predict";
     WebClient webClient = WebClient.create(url);
+    String response = webClient.post()
+            .uri("/predict")
 }
