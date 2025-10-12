1f74ce5e5905103d119a8b5c97b0b103e429df9e
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 21:28:49 +0530

    add a post mapping for the controller method

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Controller/PredictionController.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Controller/PredictionController.java
@@ -4,6 +4,7 @@ import com.example.ML.DTO.PredictionRequestDTO;
 import com.example.ML.Service.PredictionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 public class PredictionController {
     @Autowired
     private PredictionService predictionService;
+    @PostMapping("/prediction")
     public ResponseEntity<Map<String,Integer>> getPrediction(@RequestBody PredictionRequestDTO predictionRequestDTO){
         double iq = predictionRequestDTO.getIq();
         double cgpa = predictionRequestDTO.getCgpa();
