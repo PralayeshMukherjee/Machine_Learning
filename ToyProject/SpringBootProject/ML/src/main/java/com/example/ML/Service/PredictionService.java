4711f77e231f9c0ffd567257c9b0cb952b5b9be9
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 20:46:45 +0530

    create the instance of web client with the url

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
@@ -1,5 +1,8 @@
 package com.example.ML.Service;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class PredictionService {
     String url = "http://localhost:5000/predict";
+    WebClient webClient = WebClient.create(url);
 }
