5d60bbe5f995269c0c38e013272ddad0043d8993
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 21:21:05 +0530

    create a function that return the ResponseEntity and also take the request body of prediction request

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Controller/PredictionController.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Controller/PredictionController.java
@@ -1,9 +1,17 @@
 package com.example.ML.Controller;
 
+import com.example.ML.DTO.PredictionRequestDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class PredictionController {
+    public ResponseEntity<Map<String,Integer>> getPrediction(@RequestBody PredictionRequestDTO predictionRequestDTO){
+
+    }
 }
