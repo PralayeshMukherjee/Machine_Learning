27c3aeea74b483667758d004eb8bda08c9a7edf4
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 21:11:57 +0530

    create the object of prediction request dto by calling the parameterize constructor

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
@@ -1,10 +1,12 @@
 package com.example.ML.Service;
 
+import com.example.ML.DTO.PredictionRequestDTO;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class PredictionService {
     public int getPrediction(double iq,double cgpa){
+        PredictionRequestDTO predictionRequestDTO = new PredictionRequestDTO(iq,cgpa);
         String url = "http://localhost:5000";
         WebClient webClient = WebClient.create(url);
         String response = webClient.post()
