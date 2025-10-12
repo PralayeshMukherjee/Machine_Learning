da4cf08657618ca766b298e6af1b6d1379e8d4e2
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 22:51:54 +0530

    give the Cross Origin permission

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Controller/PredictionController.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Controller/PredictionController.java
@@ -4,15 +4,13 @@ import com.example.ML.DTO.PredictionRequestDTO;
 import com.example.ML.Service.PredictionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:5173")
 public class PredictionController {
     @Autowired
     private PredictionService predictionService;
