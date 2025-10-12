d67a89346bc9cd4c3da467f7b56efc8be1df1a00
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 13:08:19 +0530

    create a fucntion names predict first line means it extracts a specific piece of information from the received data second & third line I get the data fomr the json object and store it in local variables in the fourth line I convert the input data into a numpy array and reshape it to match the input format expected by the model in fifth line I use the loaded model to make a prediction based on the iput features in the last line I return the prediction result as a json response

--- ToyProject/ML_Model/mlAPI.py
+++ ToyProject/ML_Model/mlAPI.py
@@ -11,3 +11,16 @@ with open('model.pkl','rb') as file:
 # the opened file object is assigend to the variable 'file'
 
 @app.route('/predict',methods=['POST'])
+def predict():
+    data = request.get_json();
+    iq = data.get('iq')
+    cgpa = data.get('cgpa')
+    features = np.array([[iq,cgpa]]);
+    prediction = model.predict(features);
+    return jsonify({'prediction' : int(prediction[0])});
+#create a fucntion names predict 
+first line means it extracts a specific piece of information from the received data 
+second & third line I get the data fomr the json object and store it in local variables
+in the fourth line I convert the input data into a numpy array and reshape it to match the input format expected by the model
+in fifth line I use the loaded model to make a prediction based on the iput features
+in the last line I return the prediction result as a json response
