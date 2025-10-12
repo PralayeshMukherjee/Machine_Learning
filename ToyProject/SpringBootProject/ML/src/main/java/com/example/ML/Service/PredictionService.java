1a3e1094df63bc364acbf213d0ed375fa6722602
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 21:10:42 +0530

    move the code inside the function

--- ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
+++ ToyProject/SpringBootProject/ML/src/main/java/com/example/ML/Service/PredictionService.java
@@ -5,11 +5,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 public class PredictionService {
     public int getPrediction(double iq,double cgpa){
-
+        String url = "http://localhost:5000";
+        WebClient webClient = WebClient.create(url);
+        String response = webClient.post()
+                .uri("/predict")
+                .contentType(MediaType.APPLICATION_JSON)
     }
-    String url = "http://localhost:5000";
-    WebClient webClient = WebClient.create(url);
-    String response = webClient.post()
-            .uri("/predict")
-            .contentType(MediaType.APPLICATION_JSON)
 }
