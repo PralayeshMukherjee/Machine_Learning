90fc1645d1cb9988c38ad46479c5e23b00eb2528
Pralayesh Mukherjee <132552681+PralayeshMukherjee@users.noreply.github.com>
2025-10-12 12:42:34 +0530

    the open function open the model.pkl file and read it in binary mode the with...as file is a context manager that ensures thatthe file is properly closed automatically after the code block is executed the opened file object is assigend to the variable 'file'

--- ToyProject/ML_Model/mlAPI.py
+++ ToyProject/ML_Model/mlAPI.py
@@ -3,3 +3,9 @@ import pickle;
 
 app = Flask(__name__);# creatae the instance of the flask web appliction object
 
+# load the model
+with open('model.pkl','rb') as file:
+    model = pickle.load(file);
+# the open function open the model.pkl file and read it in binary mode
+# the with...as file is a context manager that ensures that the file is properly closed automatically after the code block is executed
+# the opened file object is assigend to the variable 'file'
